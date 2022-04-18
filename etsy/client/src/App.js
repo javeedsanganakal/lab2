fff150a5634f85a58df52007dc812a8334a9fc3b
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-04-18 11:26:14 -0700

    frontent

--- etsy/client/src/App.js
+++ etsy/client/src/App.js
@@ -1,56 +1,67 @@
-import React from "react"
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./Header";
-import Category from "./Category";
-import Welcome from "./Welcome";
-import Body from "./Body";
-import Home from "./Home";
-import Contact from "./Contact";
-import Footer from "./components/Footer";
-import Login from "./Login";
-import Register from "./Register";
-import ProfileDashboard from "./components/ProfileDashboard";
-import ItemsData from "./Data.json";
+import "./App.css";
+import React, { Component, useState, useEffect } from "react";
 
+// import { useState, useEffect } from "react";
+import Home from "./components/Home";
+import Signin from "./components/Signin";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Register from "./components/register";
+import WelcomePage from "./components/welcomePage";
+import Navbar from "./components/Navbar";
+import ProfilePage from "./components/profilePage";
+import SellOnEtsy from "./components/sellOnEtsy";
+import ShopHome from "./components/shopHome";
+import { useSelector } from "react-redux";
+import userSlice, { selectUser } from "./features/userSlice";
+import CheckShopName from "./components/checkShopName";
+import SearchResultItems from "./components/searchResultItems";
+// import ShopHomeByOther from "./components/shopHomeByOther";
+import ProfileForm from "./components/profileForm";
+import ProfileDashboard from "./components/profileDashboard";
+import ProductOverView from "./components/ProductOverView";
+// import CartScreen from "./components/CartScreen";
+import Cart from "./components/Cart";
+import Purchases from "./components/Purchases";
+import ProductView from "./components/ProductView";
+import ShopHomeByOther from "./components/shopHomeByOther";
 
 function App() {
-  
+  const user = useSelector(selectUser);
   return (
-    <Router> 
-      <div className="app">
-        
-        <Switch className="">
-          <Route path="/checkout">
-            <h1>
-              Checkout Page
-            </h1>
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/Register">
-            <Register/>
-          </Route>
-          <Route path="/ProfileDashboard">
-            <ProfileDashboard/>
-          </Route>
-
-          {/* This is an default page i.e., http://localhost:3000/*/}
-          <Route path="/">
-            <Header data={ItemsData}/>
-            <Category/>
-            <Welcome/>
-            <Body/>
-            <Home/>
-            <Contact/>
-            <Footer/>
-          </Route>
-        </Switch>
-        
-      </div>
-
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/updateProfile" element={<ProfileForm />} />
+          <Route path="/sellonetsy" element={<SellOnEtsy />} />
+          <Route path="/shopHome" element={<ShopHome />} />
+          {/* <Route path="/shopHome/:id" element={<ShopHome />} /> */}
+          <Route path="/shopName" element={<CheckShopName />} />
+          <Route path="/searchResults" element={<SearchResultItems />} />
+          <Route
+            path="/shopHomeForOthers/:userId/:itemId"
+            element={<ShopHomeByOther />}
+          />
+          <Route path="/profileDashboard" element={<ProfileDashboard />} />
+          <Route path="/profileForm" element={<ProfileForm />} />
+          <Route path="/productOverview" element={<ProductOverView />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/purchase" element={<Purchases />} />
+          <Route path="/productView/:id" element={<ProductView />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
+
 export default App;
--- etsy/client/src/components/Footer.js
+++ etsy/client/src/components/Footer.js
@@ -1,13 +1,79 @@
-import React from 'react';
-import "./Footer.css";
+import React from "react";
+import "./NavBar.css";
 
 function Footer() {
   return (
-    <div className="footer">
-        <p>© 2020 Etsy</p>
-        <p>Privacy · Terms · Sitemap · Company Details</p>
-    </div>
-  )
+    <footer>
+      <section class="footer-section-one">
+        <div class="content-area">
+          <div class="footer">
+            <div class="links">
+              <h3>Shop</h3>
+              <ul>
+                <li>
+                  <a href="#">Gift cards</a>
+                </li>
+                <li>
+                  <a href="#">Etsy blog</a>
+                </li>
+              </ul>
+            </div>
+            <div class="links">
+              <h3>Sell</h3>
+              <ul>
+                <li>
+                  <a href="#">Sell on Etsy</a>
+                </li>
+                <li>
+                  <a href="#">Teams</a>
+                </li>
+                <li>
+                  <a href="#">Forums</a>
+                </li>
+                <li>
+                  <a href="#">Affiliates</a>
+                </li>
+              </ul>
+            </div>
+            <div class="links">
+              <h3>About</h3>
+              <ul>
+                <li>
+                  <a href="#">Etsy, Inc.</a>
+                </li>
+                <li>
+                  <a href="#">Policies</a>
+                </li>
+                <li>
+                  <a href="#">Investors</a>
+                </li>
+                <li>
+                  <a href="#">Careers</a>
+                </li>
+                <li>
+                  <a href="#">Press</a>
+                </li>
+                <li>
+                  <a href="#">Impact</a>
+                </li>
+              </ul>
+            </div>
+            <div class="links">
+              <h3>Help</h3>
+              <ul>
+                <li>
+                  <a href="#">Help Centre</a>
+                </li>
+                <li>
+                  <a href="#">Privacy settings</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
